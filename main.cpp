ba069a3ebd1c28d108f385748f4bb2fa90a5a785
Ignust <andrey.las90@gmail.com>
2018-12-04 19:15:06 +0200

    one epollFd

--- main.cpp
+++ main.cpp
@@ -28,24 +28,46 @@ int main()
     mEvent.events = EPOLLIN;
     mEvent.data.fd = masterSocket;
     epoll_ctl(epollFd, EPOLL_CTL_ADD, masterSocket, &mEvent);
-
+    epoll_event mEvent0;
+    mEvent0.data.fd = 0;
+    mEvent0.events = EPOLLIN;
+    epoll_ctl(epollFd, EPOLL_CTL_ADD, 0, &mEvent0);
+    /*
     int epollFd_Out = epoll_create1(0);
     epoll_event mEvent_Out;
     mEvent_Out.events = EPOLLIN;
     mEvent_Out.data.fd = 0;
     epoll_ctl(epollFd_Out, EPOLL_CTL_ADD, 0, &mEvent_Out);
-
+    */
     while (1) {
 
-        Mail tempMail;
-        tempMail.data[0]=0;
-        int namber = epoll_wait(epollFd, mEvents,5, 0);
-        for(int i = 0; i < namber; ++i){
-            read(mEvents[i].data.fd, &tempMail.data, sizeof (tempMail.data));
-            cout << i << ": read: " << tempMail.data << endl;
+
+
+        int namberEpoll = epoll_wait(epollFd, mEvents,5, 0);
+        for(int i = 0; i < namberEpoll; ++i){
+            if(mEvents[i].data.fd == masterSocket){
+                Mail tempMail;
+                tempMail.data[0]=0;
+                int namberRead =read(mEvents[i].data.fd, &tempMail.data, sizeof (tempMail.data));
+                if(namberRead > 0){
+                    cout << i << ": read: " << tempMail.data << endl;
+                }
+            }
+            else {
+                Mail tempMail;
+                memset(tempMail.data, 0, sizeof(tempMail.data));
+                tempMail.data[0] = 0;
+                tempMail.typeMail = MESSAGE;
+                read(mEvents[i].data.fd, tempMail.data,sizeof(tempMail.data));
+                if(tempMail.data[0]!=0){
+                    cout <<" tempMail.data: " << tempMail.data << endl;
+                    send(masterSocket, &tempMail, sizeof (tempMail), 0);
+                }
+            }
 
         }
 
+        /*
         char buf[1024] = {0};
         int N = epoll_wait(epollFd_Out, mEvents, 5 , 0);
         for(int a =0; a < N; ++a){
@@ -60,6 +82,7 @@ int main()
             }
 
         }
+        */
     }
 
 
